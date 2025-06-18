723f2244a7ca55e1fd32132d01554de21e34cad4
kovacsg <gabor.kovacs@kuka.com>
2025-06-18 14:45:07 +0200

    ref

--- GameMotor/StoryTeller.cs
+++ GameMotor/StoryTeller.cs
@@ -1,39 +1,52 @@
-﻿using RevGameCore.Map;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StoryTeller.cs" company="KUKA Deutschland GmbH">
+//   Copyright (c) KUKA Deutschland GmbH 2006 - 2025
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace RevGameCore.GameMotor
 {
+   using RevGameCore.Map;
+
    public class StoryTeller
    {
-      private Context _context;
-      private Thread _storyThread;
-      private bool _running;
+      #region Constants and Fields
+
+      private readonly Context context;
+
+      private bool running;
+
+      private Thread storyThread;
+
+      #endregion
+
+      #region Constructors and Destructors
 
       public StoryTeller(Context context)
       {
-         _context = context;
-         _context.EnterToRoomEvent += EnterToRoomHandler;
+         this.context = context;
+         this.context.EnterToRoomEvent += EnterToRoomHandler;
       }
 
+      #endregion
+
+      #region Public Methods and Operators
 
       public void Start()
       {
-         _storyThread = new Thread(Run);
-         _storyThread.Start();
+         storyThread = new Thread(Run);
+         storyThread.Start();
       }
 
       public void Stop()
       {
-         _running = false;
-         _storyThread.Join();
+         running = false;
+         storyThread.Join();
       }
 
-      private void Run()
-      {
-         while (_running)
-         {
-            Thread.Sleep(100);
-         }
-      }
+      #endregion
+
+      #region Methods
 
       private void EnterToRoomHandler(object? sender, RoomChangeEventArgs e)
       {
@@ -41,18 +54,30 @@ namespace RevGameCore.GameMotor
          // present actual palce 
          Console.ForegroundColor = ConsoleColor.Yellow;
          Console.WriteLine("Beléptél az ajtón: " + eViaDoor.Description);
-         Console.WriteLine(_context.ActualPlace.Description);
-
-
+         Console.WriteLine(context.ActualPlace.Description);
 
          Console.ForegroundColor = ConsoleColor.Gray;
       }
+
+      private void Run()
+      {
+         while (running)
+         {
+            Thread.Sleep(100);
+         }
+      }
+
+      #endregion
    }
 
    public class RoomChangeEventArgs(IRoom roomInto, IDoor viaDoor) : EventArgs
    {
+      #region Public Properties
+
       public IRoom RoomInto { get; } = roomInto;
+
       public IDoor ViaDoor { get; } = viaDoor;
 
+      #endregion
    }
 }
